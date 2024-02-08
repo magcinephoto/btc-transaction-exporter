e3bb7def7998b920e3d9716d650282fe24764329
Mag <mag.cinephoto@gmail.com>
2024-02-08 14:03:04 +0900

    feat: add axios type

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,11 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-type-checked'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
   ignorePatterns: [
     'vite.config.ts',
     'dist/**/*.js',
--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 export const getRequest = async (url: string) => {
   try {
-    const { data } = await axios.get(url);
+    const { data } = await axios.get<AxiosResponse>(url);
     return data;
   } catch (error) {
     console.error(error);
