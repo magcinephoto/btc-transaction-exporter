4cafd84b0dac05d82b604c03bd0ccb29ad67afc0
Mag <mag.cinephoto@gmail.com>
2024-02-08 14:18:18 +0900

    feat: revert type check

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,7 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended-type-checked'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  parserOptions: {
-    project: true,
-    tsconfigRootDir: __dirname,
-  },
   ignorePatterns: [
     'vite.config.ts',
     'dist/**/*.js',
