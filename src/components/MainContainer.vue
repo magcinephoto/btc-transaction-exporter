ca5a76cde1d7c5acd4e2fc06c5e623c9d824b7e2
Mag <mag.cinephoto@gmail.com>
2024-02-08 16:06:10 +0900

    fix: add loading func

--- src/components/MainContainer.vue
+++ src/components/MainContainer.vue
@@ -5,7 +5,14 @@
     <div className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"></div>
   </div>
 
-  <button type="button" @click="fetchMEData" class="bg-slate-700 hover:bg-slate-600 text-white rounded px-4 py-2">Export Transaction CSV</button>
+  <button
+    :disabled="loading"
+    type="button"
+    @click="execMainProcess"
+    class="bg-slate-700 hover:bg-slate-600 text-white rounded px-4 py-2 focus:outline-none disabled:bg-slate-400 disabled:hover:bg-slate-400 disabled:opacity-75"
+  >
+    Export Transaction CSV
+  </button>
 </template>
 
 <script setup lang="ts">
@@ -14,7 +21,18 @@ import { fetchMEData } from '../lib/magiceden.ts';
 
 defineProps<{ msg: string }>();
 
-function execMainProcess() {
+const loading = ref(false);
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function execMainProcess() {
+  loading.value = true;
+  console.log(loading.value);
+  await sleep(5000);
+  loading.value = false;
+  console.log(loading.value);
   console.log('fire');
 }
 
