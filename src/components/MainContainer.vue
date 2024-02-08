95ef208af67f9691245e4d68e551872a18c5b22a
Mag <mag.cinephoto@gmail.com>
2024-02-08 15:54:55 +0900

    feat: add dark background

--- src/components/MainContainer.vue
+++ src/components/MainContainer.vue
@@ -1,11 +1,11 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <h1 class="text-xl font-medium text-white">{{ msg }}</h1>
 
   <div className="flex justify-center" aria-label="読み込み中">
     <div className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"></div>
   </div>
 
-  <button type="button" @click="fetchMEData">Export CSV</button>
+  <button type="button" @click="fetchMEData" class="bg-slate-700 hover:bg-slate-600 text-white rounded px-4 py-2">Export Transaction CSV</button>
 </template>
 
 <script setup lang="ts">
