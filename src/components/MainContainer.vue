8c7d9e1cbba473c15d80e0150b530e78dd6d85d9
Mag <mag.cinephoto@gmail.com>
2024-02-08 11:51:36 +0900

    feat: add button function

---
+++ src/components/MainContainer.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button type="button" @click="execMainProcess">Export CSV</button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>();
+
+function execMainProcess() {
+  console.log('fire');
+}
+
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
