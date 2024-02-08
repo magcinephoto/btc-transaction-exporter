eb965e5f3b85b083b4bb3b9e9ff1edfc91406112
Mag <mag.cinephoto@gmail.com>
2024-02-08 14:33:17 +0900

    feat: add tailwind

--- src/components/MainContainer.vue
+++ src/components/MainContainer.vue
@@ -1,5 +1,10 @@
 <template>
   <h1>{{ msg }}</h1>
+
+  <div className="flex justify-center" aria-label="読み込み中">
+    <div className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"></div>
+  </div>
+
   <button type="button" @click="fetchMEData">Export CSV</button>
 </template>
 
