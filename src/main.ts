03cea82a101f88863a49a24de59090d6305fa104
Mag <mag.cinephoto@gmail.com>
2024-02-11 07:07:38 +0900

    feat: limit icon import

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import App from './App.vue'
 import './index.css'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { far } from '@fortawesome/free-regular-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-library.add(far, fas)
+import { faCircleQuestion } from '@fortawesome/free-regular-svg-icons'
+import { faUser, faCircleDollarToSlot } from '@fortawesome/free-solid-svg-icons'
+library.add(faCircleQuestion, faUser, faCircleDollarToSlot)
 
 const app = createApp(App)
 app.component('font-awesome-icon', FontAwesomeIcon)
